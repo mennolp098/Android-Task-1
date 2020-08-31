f9b5013560560b0a1ee40ffcecb4b1972c5145bf
Menno <run_jongejan@hotmail.com>
2020-08-31 19:31:57 +0200

    displaying dice number

--- app/src/main/java/com/example/android_task_1/MainActivity.kt
+++ app/src/main/java/com/example/android_task_1/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val MAX_NUMBER:Int = 6;
     private val MIN_NUMBER:Int = 0;
+    private var diceNumber:Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +33,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice(min:Int, max:Int) {
         val dice = (Math.random() * ((max - min) + 1)) + min
-        println(dice);
+        this.diceNumber = dice.toInt()
+
+        displayDiceNumber()
+    }
+
+    private fun displayDiceNumber() {
+        binding.diceTextView.text = this.diceNumber.toString()
     }
 
 }

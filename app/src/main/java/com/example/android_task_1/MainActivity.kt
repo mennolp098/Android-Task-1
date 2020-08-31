4ae429c953c97dfce9e88c5d0ea63c4e6fc82a51
Menno <run_jongejan@hotmail.com>
2020-08-31 19:27:35 +0200

    Added event listeners on buttons, set some constants and also created a start on rolling the dice

--- app/src/main/java/com/example/android_task_1/MainActivity.kt
+++ app/src/main/java/com/example/android_task_1/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.android_task_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_task_1.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val MAX_NUMBER:Int = 6;
+    private val MIN_NUMBER:Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) // Sets the activity layout
+
+        setButtonListeners();
     }
+
+    private fun setButtonListeners() {
+        binding.rollHighButton.setOnClickListener {
+            rollDice(MIN_NUMBER, MAX_NUMBER)
+        }
+        binding.rollLowButton.setOnClickListener {
+            rollDice(MIN_NUMBER, MAX_NUMBER)
+        }
+        binding.rollHighButton.setOnClickListener {
+            rollDice(MIN_NUMBER, MAX_NUMBER)
+        }
+    }
+
+    private fun rollDice(min:Int, max:Int) {
+        val dice = (Math.random() * ((max - min) + 1)) + min
+        println(dice);
+    }
+
 }

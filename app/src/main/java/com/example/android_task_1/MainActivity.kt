7e39f6e97bf06dbae08b6e163d9ee4fe8bc133f1
Menno <run_jongejan@hotmail.com>
2020-08-31 19:57:16 +0200

    Added win and loss condition

--- app/src/main/java/com/example/android_task_1/MainActivity.kt
+++ app/src/main/java/com/example/android_task_1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.android_task_1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.android_task_1.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val MAX_NUMBER:Int = 6;
     private val MIN_NUMBER:Int = 0;
+    private enum class DiceExpectation {
+        LOWER, SAME, HIGHER
+    }
     private var diceNumber:Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,25 +26,58 @@ class MainActivity : AppCompatActivity() {
 
     private fun setButtonListeners() {
         binding.rollHighButton.setOnClickListener {
-            rollDice(MIN_NUMBER, MAX_NUMBER)
+            rollDice(DiceExpectation.HIGHER)
         }
         binding.rollLowButton.setOnClickListener {
-            rollDice(MIN_NUMBER, MAX_NUMBER)
+            rollDice(DiceExpectation.LOWER)
         }
-        binding.rollHighButton.setOnClickListener {
-            rollDice(MIN_NUMBER, MAX_NUMBER)
+        binding.rollSameButton.setOnClickListener {
+            rollDice(DiceExpectation.SAME)
         }
     }
 
-    private fun rollDice(min:Int, max:Int) {
-        val dice = (Math.random() * ((max - min) + 1)) + min
-        this.diceNumber = dice.toInt()
+    private fun rollDice(diceExpectation: DiceExpectation) {
+        updateDiceNumber()
+        val dice = (1..6).random()
+        val hasWon = hasWonDiceRoll(this.diceNumber, dice.toInt(), diceExpectation);
+        this.diceNumber = dice
 
-        displayDiceNumber()
+        updateUI(hasWon)
     }
 
-    private fun displayDiceNumber() {
-        binding.diceTextView.text = this.diceNumber.toString()
+    private fun updateUI(hasWon: Boolean)
+    {
+        updateDiceImage()
+        displaySuccessOrFailed(hasWon)
+    }
+
+    private fun updateDiceImage() {
+
+    }
+
+    private fun hasWonDiceRoll(previousDice: Int, currentDice: Int, diceExpectation: DiceExpectation):Boolean {
+        if(diceExpectation == DiceExpectation.HIGHER)
+        {
+            return (previousDice < currentDice)
+        } else if(diceExpectation == DiceExpectation.LOWER) {
+            return (previousDice > currentDice)
+        }
+
+        return (previousDice == currentDice)
+    }
+
+    private fun updateDiceNumber() {
+        binding.diceTextView.text = getString(R.string.you_rolled, this.diceNumber)
+    }
+
+    private fun displaySuccessOrFailed(hasWon: Boolean) {
+        if(hasWon)
+        {
+            Toast.makeText(this, R.string.you_won, Toast.LENGTH_LONG).show()
+            return
+        }
+        Toast.makeText(this, R.string.you_lost, Toast.LENGTH_LONG).show()
+
     }
 
 }
